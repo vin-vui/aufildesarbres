272d4676df6d063c99f83858fcae75080889c108
vinvui <vincent.vuillemin@gmail.com>
2021-06-07 14:13:31 +0200

    facebook link

--- resources/views/web/guest/homepage/partials/contact.blade.php
+++ resources/views/web/guest/homepage/partials/contact.blade.php
@@ -61,8 +61,7 @@
                     </dl>
                     <ul class="mt-8 flex space-x-12" role="list">
                         <li>
-                            <a class="text-blue-th hover:text-indigo-100" href="#">
-                                <span class="sr-only">Facebook</span>
+                            <a class="text-blue-th hover:text-indigo-100" target="_blank" href="https://www.facebook.com/aufildesarbres/">
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" aria-hidden="true">
                                     <path d="M22.258 1H2.242C1.556 1 1 1.556 1 2.242v20.016c0 .686.556 1.242 1.242 1.242h10.776v-8.713h-2.932V11.39h2.932V8.887c0-2.906 1.775-4.489 4.367-4.489 1.242 0 2.31.093 2.62.134v3.037l-1.797.001c-1.41 0-1.683.67-1.683 1.653v2.168h3.362l-.438 3.396h-2.924V23.5h5.733c.686 0 1.242-.556 1.242-1.242V2.242C23.5 1.556 22.944 1 22.258 1" fill="currentColor" />
                                 </svg>
