09ddbfca8a3d49f767eefeb0bff3e0816b1a187e
vinvui <vincent.vuillemin@gmail.com>
2021-06-18 14:03:57 +0200

    shadow on team

--- resources/views/web/guest/homepage/partials/team.blade.php
+++ resources/views/web/guest/homepage/partials/team.blade.php
@@ -1,6 +1,6 @@
 
 <section class="bg-white">
-  <div class="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
+  <div class="px-4 py-12 mx-auto text-center max-w-7xl sm:px-6 lg:px-8 lg:py-24">
     <div class="space-y-12">
 
       <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
@@ -12,9 +12,9 @@
 
         <li>
           <div class="space-y-6">
-            <img class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src="{{ url('img/paul.png') }}" alt="">
+            <img class="w-40 h-40 mx-auto rounded-full shadow-lg xl:w-56 xl:h-56" src="{{ url('img/paul.png') }}" alt="">
             <div class="space-y-2">
-              <div class="text-lg leading-6 font-medium space-y-1">
+              <div class="space-y-1 text-lg font-medium leading-6">
                 <h3>Paul</h3>
               </div>
             </div>
@@ -23,9 +23,9 @@
 
         <li>
           <div class="space-y-6">
-            <img class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src="{{ url('img/rachel.png') }}" alt="">
+            <img class="w-40 h-40 mx-auto rounded-full shadow-lg xl:w-56 xl:h-56" src="{{ url('img/rachel.png') }}" alt="">
             <div class="space-y-2">
-              <div class="text-lg leading-6 font-medium space-y-1">
+              <div class="space-y-1 text-lg font-medium leading-6">
                 <h3>Rachel</h3>
               </div>
             </div>
@@ -34,9 +34,9 @@
 
         <li>
           <div class="space-y-6">
-            <img class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src="{{ url('img/pierre-antoine.png') }}" alt="">
+            <img class="w-40 h-40 mx-auto rounded-full shadow-lg xl:w-56 xl:h-56" src="{{ url('img/pierre-antoine.png') }}" alt="">
             <div class="space-y-2">
-              <div class="text-lg leading-6 font-medium space-y-1">
+              <div class="space-y-1 text-lg font-medium leading-6">
                 <h3>Pierre-Antoine </h3>
               </div>
             </div>
