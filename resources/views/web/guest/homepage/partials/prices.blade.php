0a455f202619cbc0cdf71c3b6bb2792df70d6188
vinvui <vincent.vuillemin@gmail.com>
2021-06-21 14:23:00 +0200

    fix some visuals

--- resources/views/web/guest/homepage/partials/prices.blade.php
+++ resources/views/web/guest/homepage/partials/prices.blade.php
@@ -1,20 +1,20 @@
 <section id="tarifs" class="py-32">
-    <div class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+    <div class="max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
         <div class="lg:grid lg:grid-cols-3 lg:gap-24 lg:items-center">
 
             <div>
-                <h2 class="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl pointer-events-none">
+                <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 pointer-events-none sm:text-4xl">
                     Tarifs
                 </h2>
-                <p class="mt-6 max-w-3xl text-lg leading-7 text-gray-500 pointer-events-none">
+                <p class="max-w-3xl mt-6 text-lg leading-7 text-gray-500 pointer-events-none">
                     Tarifs préférentiels sur demande pour les groupes, les clubs, les colonies de vacances, les scolaires, etc.
                 </p>
 
                 <div class="mt-6">
                     <div class="inline-flex rounded-md shadow">
-                        <a href="#contact" class="group inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-th hover:bg-gray-50 hover:text-black-th">
+                        <a href="#contact" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white border border-transparent rounded-md group bg-blue-th hover:bg-gray-50 hover:text-black-th">
                             Contactez nous
-                            <svg xmlns="http://www.w3.org/2000/svg" class="-mr-1 ml-3 h-5 w-5 text-white group-hover:text-black-th" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-3 -mr-1 text-white group-hover:text-black-th" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                             </svg>
                         </a>
@@ -22,8 +22,8 @@
                 </div>
 
                 <div class="flex mt-8 pointer-events-none">
-                    <div class="mr-4 flex-shrink-0 self-start">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-green-th" viewBox="0 0 20 20" fill="currentColor">
+                    <div class="self-start flex-shrink-0 mr-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-16 h-16 text-green-th" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
                         </svg>
                     </div>
@@ -32,8 +32,8 @@
                         <p class="mt-1">
                             -10% sur présentation de la carte Avantage Jeune
                         </p>
-                        <p class="mt-4 text-xs flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 pr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <p class="flex mt-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 pr-2 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                             </svg>
                             Pas de réduction sur les forfaits à 3 et 10 euros.
@@ -45,32 +45,32 @@
 
             </div>
 
-            <div class="mt-12 col-span-2 pointer-events-none">
-                <div class="bg-white shadow-2xl overflow-hidden rounded-lg">
+            <div class="col-span-2 mt-12 pointer-events-none">
+                <div class="overflow-hidden bg-white rounded-lg shadow-2xl">
                     <ul class="divide-y divide-gray-200">
 
                         @foreach (App\Models\Price::all() as $price)
 
                         <li>
-                            <div class="min-w-0 p-4 items-center grid grid-cols-12 sm:gap-4 gap-1">
-                                <div class="col-span-1 sm:block hidden">
-                                    <img class="h-12 w-12 rounded-full" src="{{ url('storage/'.$price->image) }}" alt="">
+                            <div class="grid items-center min-w-0 grid-cols-12 gap-1 p-4 sm:gap-4">
+                                <div class="hidden col-span-1 sm:block">
+                                    <img class="w-12 h-12 rounded-full" src="{{ url('storage/'.$price->image) }}" alt="">
                                 </div>
                                 <div class="col-span-7">
-                                    <p class="sm:text-2xl text-lg font-medium text-black-th">
+                                    <p class="text-lg font-medium sm:text-2xl text-black-th">
                                         {{ $price->public }}
                                         @if ($price->description != null)
-                                        <span class="text-sm pl-2 hidden sm:inline-block">{{ $price->description }}</span>
+                                        <span class="hidden pl-2 text-sm sm:inline-block">{{ $price->description }}</span>
                                         @endif
                                     </p>
                                 </div>
-                                <div class="sm:col-span-4 col-span-5">
-                                    <p class="sm:text-2xl text-lg font-bold text-green-th flex justify-end">{{ $price->price }}</p>
+                                <div class="col-span-5 sm:col-span-4">
+                                    <p class="flex justify-end text-lg font-bold sm:text-2xl text-green-th">{{ $price->price }}</p>
                                 </div>
 
                                 @if ($price->description != null)
                                 <div class="col-span-12 sm:hidden">
-                                    <span class="text-sm pl-2 flex justify-start">{{ $price->description }}</span>
+                                    <span class="flex justify-start pl-2 text-sm">{{ $price->description }}</span>
                                 </div>
                                 @endif
 
@@ -89,7 +89,7 @@
 
 </section>
 
-<div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+<div class="relative max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
     <div class="absolute inset-0 flex items-center" aria-hidden="true">
         <div class="w-full border-t border-gray-300"></div>
     </div>
--- resources/views/web/guest/navigation-menu.blade.php
+++ resources/views/web/guest/navigation-menu.blade.php
@@ -1,24 +1,24 @@
-<div class="bg-gradient-to-r from-green-th to-blue-th fixed z-50 right-0 top-0 w-full shadow-2xl" x-data="{ open: false }">
+<div class="fixed top-0 right-0 z-50 w-full shadow-2xl bg-gradient-to-r from-green-th to-blue-th" x-data="{ open: false }">
 
-    <div class="max-w-7xl mx-auto px-4 sm:px-6">
-        <div class="flex justify-between items-center py-2 md:justify-start md:space-x-10">
+    <div class="px-4 mx-auto max-w-7xl sm:px-6">
+        <div class="flex items-center justify-between py-2 md:justify-start md:space-x-10">
 
             <div class="flex justify-start">
                 <a href="#">
-                    <img class="h-12 w-auto sm:h-16" src="{{ url('img/logo.png') }}" alt="">
+                    <img class="w-auto h-12 sm:h-16" src="{{ url('img/logo.png') }}" alt="">
                 </a>
             </div>
 
-            <div class="-mr-2 -my-2 md:hidden">
-                <button @click="open = !open" type="button" class="bg-transparent rounded-md p-2 inline-flex items-center justify-center text-white hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-th">
+            <div class="-my-2 -mr-2 md:hidden">
+                <button @click="open = !open" type="button" class="inline-flex items-center justify-center p-2 text-white bg-transparent rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-th">
                     <span class="sr-only">Open menu</span>
-                    <svg class="h-6 w-6" x-description="Heroicon name: outline/menu" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <svg class="w-6 h-6" x-description="Heroicon name: outline/menu" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                     </svg>
                 </button>
             </div>
 
-            <div class="hidden md:flex items-center justify-end md:flex-1 space-x-10 lg:w-0">
+            <div class="items-center justify-end hidden space-x-10 md:flex md:flex-1 lg:w-0">
                 <a href="{{ route('welcome') }}" class="text-base font-medium text-white hover:text-black-th">
                     Accueil
                 </a>
@@ -41,14 +41,14 @@
 
                     <a href="https://www.facebook.com/aufildesarbres/" class="text-black-th hover:text-white">
                         <span class="sr-only">Facebook</span>
-                        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                             <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path>
                         </svg>
                     </a>
 
-                    <a href="#" class="text-black-th hover:text-white">
+                    <a href="https://www.instagram.com/parcaventuremalbuisson/" class="text-black-th hover:text-white">
                         <span class="sr-only">Instagram</span>
-                        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                             <path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd"></path>
                         </svg>
                     </a>
@@ -58,17 +58,17 @@
         </div>
     </div>
 
-    <div x-show="open" x-transition:enter="duration-200 ease-out" x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="duration-100 ease-in" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95" x-description="Mobile menu, show/hide based on mobile menu state." class="absolute top-0 z-50 inset-x-0 p-2 transition transform origin-top-right md:hidden" x-ref="panel" @click.away="open = false">
-        <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-            <div class="pt-5 pb-6 px-5">
+    <div x-show="open" x-transition:enter="duration-200 ease-out" x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="duration-100 ease-in" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95" x-description="Mobile menu, show/hide based on mobile menu state." class="absolute inset-x-0 top-0 z-50 p-2 transition origin-top-right transform md:hidden" x-ref="panel" @click.away="open = false">
+        <div class="bg-white divide-y-2 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 divide-gray-50">
+            <div class="px-5 pt-5 pb-6">
                 <div class="flex items-center justify-between">
                     <div>
-                        <img class="h-8 w-auto" src="{{ url('img/logo.png') }}" alt="Workflow">
+                        <img class="w-auto h-8" src="{{ url('img/logo.png') }}" alt="Workflow">
                     </div>
                     <div class="-mr-2">
-                        <button @click="open = !open" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-th">
+                        <button @click="open = !open" type="button" class="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-th">
                             <span class="sr-only">Close menu</span>
-                            <svg class="h-6 w-6" x-description="Heroicon name: outline/x" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <svg class="w-6 h-6" x-description="Heroicon name: outline/x" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                             </svg>
                         </button>
@@ -77,8 +77,8 @@
                 <div class="mt-6">
                     <nav class="grid gap-y-8">
 
-                        <a href="{{ route('welcome') }}" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                            <svg class="flex-shrink-0 h-6 w-6 text-blue-th" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <a href="{{ route('welcome') }}" class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50">
+                            <svg class="flex-shrink-0 w-6 h-6 text-blue-th" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                             </svg>
                             <span class="ml-3 text-base font-medium text-gray-900">
@@ -86,8 +86,8 @@
                             </span>
                         </a>
 
-                        <a @click="open = !open" href="{{ route('welcome') }}/#parcours" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                            <svg class="flex-shrink-0 h-6 w-6 text-blue-th" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <a @click="open = !open" href="{{ route('welcome') }}/#parcours" class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50">
+                            <svg class="flex-shrink-0 w-6 h-6 text-blue-th" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
                             </svg>
                             <span class="ml-3 text-base font-medium text-gray-900">
@@ -95,8 +95,8 @@
                             </span>
                         </a>
 
-                        <a @click="open = !open" href="{{ route('welcome') }}/#tarifs" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                            <svg class="flex-shrink-0 h-6 w-6 text-blue-th" mlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <a @click="open = !open" href="{{ route('welcome') }}/#tarifs" class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50">
+                            <svg class="flex-shrink-0 w-6 h-6 text-blue-th" mlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
                             </svg>
                             <span class="ml-3 text-base font-medium text-gray-900">
@@ -104,8 +104,8 @@
                             </span>
                         </a>
 
-                        <a @click="open = !open" href="{{ route('welcome') }}/#horaires" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                            <svg class="flex-shrink-0 h-6 w-6 text-blue-th" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <a @click="open = !open" href="{{ route('welcome') }}/#horaires" class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50">
+                            <svg class="flex-shrink-0 w-6 h-6 text-blue-th" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
                             <span class="ml-3 text-base font-medium text-gray-900">
@@ -113,8 +113,8 @@
                             </span>
                         </a>
 
-                        <a @click="open = !open" href="{{ route('welcome') }}/#photos" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                            <svg class="flex-shrink-0 h-6 w-6 text-blue-th" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <a @click="open = !open" href="{{ route('welcome') }}/#photos" class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50">
+                            <svg class="flex-shrink-0 w-6 h-6 text-blue-th" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
                             </svg>
@@ -123,8 +123,8 @@
                             </span>
                         </a>
 
-                        <a @click="open = !open" href="{{ route('welcome') }}/#contact" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                            <svg class="flex-shrink-0 h-6 w-6 text-blue-th" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <a @click="open = !open" href="{{ route('welcome') }}/#contact" class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50">
+                            <svg class="flex-shrink-0 w-6 h-6 text-blue-th" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                             </svg>
                             <span class="ml-3 text-base font-medium text-gray-900">
@@ -136,14 +136,14 @@
 
                             <a href="https://www.facebook.com/aufildesarbres/" class="text-black-th hover:text-white">
                                 <span class="sr-only">Facebook</span>
-                                <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path>
                                 </svg>
                             </a>
 
                             <a href="https://www.instagram.com/parcaventuremalbuisson/" class="text-black-th hover:text-white">
                                 <span class="sr-only">Instagram</span>
-                                <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd"></path>
                                 </svg>
                             </a>
