c92ec824e03191a0c924476a12c98fa254cac3b7
vinvui <admin@vinvui.com>
2023-04-17 13:04:09 +0200

    Change UI + Add real pdf

--- resources/views/web/guest/homepage/partials/news.blade.php
+++ resources/views/web/guest/homepage/partials/news.blade.php
@@ -10,7 +10,7 @@
                         <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M4 16.5V20a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-3.5M3 14v-1a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v1m-9-6v3m0-3c1.262 0 2-.968 2-2.625S12 2 12 2s-2 1.718-2 3.375S10.738 8 12 8Z"/><path d="M9 14a3 3 0 1 1-6 0m12 0a3 3 0 1 1-6 0m12 0a3 3 0 1 1-6 0"/></g>
                     </svg>
                 </div>
-                <h3 class="text-xl font-medium text-black-th pointer-events-none">Joyeux Anniversaire !</h3>
+                <h3 class="text-xl font-medium text-black-th pointer-events-none">Evénement Spécial</h3>
                 <p class="mt-4 text-base text-gray-500 pointer-events-none">
                     Viens fêter ton anniversaire au parc ! Avec ce bon d'invitation pour tes amis ! (à imprimer)
                 </p>
