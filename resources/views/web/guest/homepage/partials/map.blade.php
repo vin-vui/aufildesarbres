275cb61c5d8d25a338a25020acb1818220a8184a
vinvui <admin@vinvui.com>
2024-02-05 14:04:41 +0100

    Mise à jour de la carte et des prix

--- resources/views/web/guest/homepage/partials/map.blade.php
+++ resources/views/web/guest/homepage/partials/map.blade.php
@@ -1,5 +1,5 @@
 <section class="">
     <div class="relative">
-        <iframe class="w-full" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10924.578399260992!2d6.312918978820795!3d46.8014574597317!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x71e8556d7a9788be!2sParc%20Aventure%20Malbuisson!5e0!3m2!1sfr!2sfr!4v1622560883762!5m2!1sfr!2sfr" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe class="w-full" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3247.7795992212396!2d6.317540255616183!3d46.80339539062011!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDbCsDQ4JzExLjQiTiA2wrAxOScxMy4yIkU!5e0!3m2!1sfr!2sfr!4v1707138137069!5m2!1sfr!2sfr" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 </section>
--- resources/views/web/guest/homepage/partials/prices.blade.php
+++ resources/views/web/guest/homepage/partials/prices.blade.php
@@ -51,7 +51,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 pr-2 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                             </svg>
-                            Pas de réduction sur les forfaits à 3 et 10 euros.
+                            Pas de réduction sur les forfaits à 13 euros.
                         </p>
                     </div>
                 </div>
